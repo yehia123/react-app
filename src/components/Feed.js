cee4ffa178b7a8f98e3704d360200e924581caa9
Yehia Qtaish <yehiaqtaish@gmail.com>
2020-02-09 22:37:45 -0800

    Added some text

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -11,7 +11,8 @@ export const Feed = () => (
     <div className="row">
       <div className="col-sm box">
         <ul class="list-group">
-          <li class="list-group-item active">Submissions</li>
+          <li class="list-group-item active">Click A Submission</li>
+          <li class="list-group-item">Submissions</li>
           <li class="list-group-item">Traingle</li>
           <li class="list-group-item">Arm bar</li>
           <li class="list-group-item">Foot lock</li>
@@ -22,10 +23,20 @@ export const Feed = () => (
       </div>
       <div className="col-sm box">
         <ul class="list-group">
-          <li class="list-group-item active">Counters</li>
-          <li class="list-group-item">Knee Push knee over</li>
-          <li class="list-group-item">Rear Naked Hand Motion</li>
-          <li class="list-group-item">Shove Knee Inside</li>
+          <li class="list-group-item text-center active">
+            BJJ Rule(s) of thumb
+          </li>
+          <li class="list-group-item">Setups, setups, setups.</li>
+          <li class="list-group-item">Move yourself, not the other person.</li>
+          <li class="list-group-item">
+            When passing guard, keep their feet off of you at all times.
+          </li>
+          <li class="list-group-item">Posture control = Guard retention.</li>
+          <li class="list-group-item">
+            keep your enemies close, keep your elbows closer
+          </li>
+          <li class="list-group-item">Fight grips.</li>
+          <li class="list-group-item">The guard is a battle of posture.</li>
         </ul>
       </div>
     </div>
